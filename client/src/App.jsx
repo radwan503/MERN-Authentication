4c3bdeedfe1138755be165892d085b6a4ce06a17
20240649 <radwan.ahmed@thecitybank.com>
2024-09-21 13:45:33 +0600

    create routes and pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+        <Route path='/sign-in' element={<SignIn/>}/>
+        <Route path='/sing-up' element={<SignUp/>}/>
+      </Routes>
+  </BrowserRouter>
   )
 }
 
